0b6f14e473885dfd664d3aa98ec8c2d9ab65b2ac
Joaquin Ponce de Leon <joacoponce1@gmail.com>
2026-05-06 13:10:31 -0300

    feat: cambio de keycloak por entra microsoft azure

--- proyectoIntegradorBack/src/main/java/com/co2x/dmrv/controller/PaqueteController.java
+++ proyectoIntegradorBack/src/main/java/com/co2x/dmrv/controller/PaqueteController.java
@@ -1,4 +1,20 @@
 package com.co2x.dmrv.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paquetes")
 public class PaqueteController {
+    @GetMapping
+    public String listar() {
+        return "Listado de paquetes";
+    }
+
+    @PostMapping
+    public String crear() {
+        return "Paquete creado";
+    }
 }
