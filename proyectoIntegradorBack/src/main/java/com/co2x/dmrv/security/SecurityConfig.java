93911b49388ddafbc6f882b3b46743fade12e7f9
constanzaesquivel <cotiesquiveln@gmail.com>
2026-05-08 16:57:01 +0200

    arreglos para entra id config

--- proyectoIntegradorBack/src/main/java/com/co2x/dmrv/security/SecurityConfig.java
+++ proyectoIntegradorBack/src/main/java/com/co2x/dmrv/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/test/publico").permitAll()
                         .anyRequest().authenticated()
                 )
 
