7f544f43369a15646d40899668eedae2492d528f
Joaquin Ponce de Leon <joacoponce1@gmail.com>
2026-05-08 11:17:49 -0300

    cambios IAM

--- proyectoIntegradorBack/src/main/java/com/co2x/dmrv/controller/TestController.java
+++ proyectoIntegradorBack/src/main/java/com/co2x/dmrv/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.co2x.dmrv.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/public")
+    @GetMapping("/publico")
     public String publico() {
         return "Endpoint público";
     }
 
-    @GetMapping("/privado")
-    public String privado() {
-        return "Endpoint privado";
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String admin() {
+        return "Hola Admin";
+    }
+
+    @GetMapping("/empleado")
+    @PreAuthorize("hasRole('EMPLEADO')")
+    public String empleado() {
+        return "Hola Empleado";
+    }
+
+    @GetMapping("/auditor")
+    @PreAuthorize("hasRole('AUDITOR')")
+    public String auditor() {
+        return "Hola Auditor";
     }
 }
