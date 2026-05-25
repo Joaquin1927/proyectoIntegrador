e26d779b2407e1e46044d517138d7e007d817ec8
Constanza Esquivel <cotiesquiveln@gmail.com>
2026-05-25 23:39:46 +0200

    Revert frontend to commit 430a976

--- proyectoIntegradorFront/frontend/src/main.jsx
+++ proyectoIntegradorFront/frontend/src/main.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
 import App from "./App";
-import { msalConfig } from "./authConfig";
-
-const msalInstance = new PublicClientApplication(msalConfig);
+import "./styles.css";
+import { AppProvider } from "./context/AppContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <MsalProvider instance={msalInstance}>
-    <App />
-  </MsalProvider>
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>
 );
-
