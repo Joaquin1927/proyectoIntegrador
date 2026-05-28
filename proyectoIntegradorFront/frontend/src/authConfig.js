590aa46fd39fc618410b410c8aef95b8e60bfcc2
Joaquin Ponce de Leon <joacoponce1@gmail.com>
2026-05-28 15:10:35 -0300

    fix frontend auth

--- proyectoIntegradorFront/frontend/src/authConfig.js
+++ proyectoIntegradorFront/frontend/src/authConfig.js
@@ -1,13 +1,16 @@
-export const msalConfig = {
+import { PublicClientApplication } from "@azure/msal-browser";
+
+const msalConfig = {
   auth: {
     clientId: "36920833-e50a-48be-b51a-e363b373c011",
     authority:
       "https://login.microsoftonline.com/cbb64725-bd29-4b87-93b2-e5fcf09ff37f",
-    redirectUri:
-      "https://calm-flower-0a1cfea0f.7.azurestaticapps.net",
+    redirectUri: "https://calm-flower-0a1cfea0f.7.azurestaticapps.net",
   },
 };
 
 export const loginRequest = {
   scopes: ["api://36920833-e50a-48be-b51a-e363b373c011/.default"],
 };
+
+export const msalInstance = new PublicClientApplication(msalConfig);
