6056b0b5b8d3cfc3798eaa8bb4e503a3cee62d21
Joaquin Ponce de Leon <joacoponce1@gmail.com>
2026-05-28 15:05:19 -0300

    error front arreglado

--- proyectoIntegradorFront/frontend/src/main.jsx
+++ proyectoIntegradorFront/frontend/src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-
-import { PublicClientApplication } from "@azure/msal-browser";
+import { BrowserRouter } from "react-router-dom";
 import { MsalProvider } from "@azure/msal-react";
 
-import { msalConfig } from "./authConfig";
-
-const msalInstance = new PublicClientApplication(msalConfig);
+import App from "./App";
+import { msalInstance } from "./authConfig";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <MsalProvider instance={msalInstance}>
-    <App />
-  </MsalProvider>
+  <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MsalProvider>
+  </React.StrictMode>
 );
