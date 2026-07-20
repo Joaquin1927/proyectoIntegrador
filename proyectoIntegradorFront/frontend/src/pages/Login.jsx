10fe32833ae69b34575d7e9dd55e1fef327ce922
bcharletto <brunocharletto@gmail.com>
2026-07-20 19:24:00 -0300

    listar empresas listo

--- proyectoIntegradorFront/frontend/src/pages/Login.jsx
+++ proyectoIntegradorFront/frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
   return (
     <section className="panel">
       <h1>Login</h1>
-      <h1 style={{ color: "red" }}>DEPLOY TEST 20 JULIO - 14:45</h1>
+      <h1 style={{ color: "red" }}>DEPLOY TEST 20 JULIO - 19:20</h1>
       <div className="actions">
         <button className="primary" onClick={submit}>
           Iniciar sesión con Microsoft
