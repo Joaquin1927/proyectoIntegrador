d94ff7aa086de4a3db06e1ff1e896e0bc6f0f4d9
Joaquin Ponce de Leon <joacoponce1@gmail.com>
2026-07-20 14:22:41 -0300

    page registrarEmpresa arreglada

--- proyectoIntegradorFront/frontend/src/pages/Notificaciones.jsx
+++ proyectoIntegradorFront/frontend/src/pages/Notificaciones.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { useApp } from "../context/AppContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useMsal } from "@azure/msal-react";
 
 export default function Notificaciones() {
   const API = import.meta.env.VITE_API_URL;
+  const { instance, accounts } = useMsal();
   const { user } = useApp();
   const navigate = useNavigate();
 
@@ -12,12 +14,25 @@ export default function Notificaciones() {
 
   useEffect(() => {
     if (!user?.email) return;
-
-    axios
-      .get(`${API}/notificaciones/${user.email}`)
-      .then((res) => setNotificaciones(res.data))
-      .catch(console.error);
-  }, [user]);
+    const cargarNotificaciones = async () => {
+      try {
+        const response = await instance.acquireTokenSilent({
+          scopes: [import.meta.env.VITE_SCOPE],
+          account: accounts[0],
+        });
+        const token = response.accessToken;
+        const res = await axios.get(`${API}/notificaciones/${user.email}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setNotificaciones(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    cargarNotificaciones();
+  }, [user, instance, accounts]);
 
   if (!user) return <p>Cargando...</p>;
 
--- proyectoIntegradorFront/frontend/src/pages/RegistrarEmpresa.jsx
+++ proyectoIntegradorFront/frontend/src/pages/RegistrarEmpresa.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { useMsal } from "@azure/msal-react";
+import axios from "axios";
 export default function RegistrarEmpresa() {
+  const API = import.meta.env.VITE_API_URL;
+
   const [nombre, setNombre] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
-
+  const { instance, accounts } = useMsal();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -22,25 +25,27 @@ export default function RegistrarEmpresa() {
     try {
       setLoading(true);
 
-      const res = await fetch("http://localhost:8080/empresas", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nombre }),
+      const response = await instance.acquireTokenSilent({
+        scopes: [import.meta.env.VITE_SCOPE],
+        account: accounts[0],
       });
+      const token = response.accessToken;
+      const res = await axios.post(
+        `${API}/empresas`,
+        { nombre },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        },
+      );
 
-      if (!res.ok) {
-        const msg = await res.text();
-        throw new Error(msg || "Error al registrar empresa");
-      }
-
-      const data = await res.json();
+      const data = res.data;
       setSuccess(`Empresa '${data.nombre}' registrada correctamente`);
 
       // Opcional: redirigir después de 1.5s
       setTimeout(() => navigate("/dashboard"), 1500);
-
     } catch (err) {
       setError(err.message);
     } finally {
