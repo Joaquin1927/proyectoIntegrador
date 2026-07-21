c9f044d5f25d8b9a3552fe9527d28474b30333e4
bcharletto <brunocharletto@gmail.com>
2026-07-21 19:23:24 -0300

    resolucion de errores en registrar paquete

--- proyectoIntegradorFront/frontend/src/main.jsx
+++ proyectoIntegradorFront/frontend/src/main.jsx
@@ -6,23 +6,19 @@ import { MsalProvider } from "@azure/msal-react";
 import App from "./App";
 import { msalInstance } from "./auth/msalConfig";
 import { AppProvider } from "./context/AppProvider";
+import { ToasterProvider } from "./ui/Toaster";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* ✅ MSAL manejado globalmente */}
     <MsalProvider instance={msalInstance}>
-      
-      {/* ✅ Contexto global de app */}
       <AppProvider>
-        
-        {/* ✅ Router */}
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-
+        <ToasterProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ToasterProvider>
       </AppProvider>
-
     </MsalProvider>
   </React.StrictMode>
 );
